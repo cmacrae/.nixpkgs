8cdc3efaf5c99748fa783721028e20f5119a1a49
cmacrae <calum0macrae@gmail.com>
2018-02-13 15:56:51 +0000

    Pkg: add bash completion fetching to custom Vault package

--- pkgs/vault/default.nix
+++ pkgs/vault/default.nix
@@ -5,7 +5,15 @@
 # Reported: https://github.com/NixOS/nixpkgs/issues/34934
 with import <nixpkgs> {};
 
-stdenv.mkDerivation rec {
+let
+  vaultBashCompletions = fetchFromGitHub {
+    owner = "iljaweis";
+    repo = "vault-bash-completion";
+    rev = "e2f59b64be1fa5430fa05c91b6274284de4ea77c";
+    sha256 = "10m75rp3hy71wlmnd88grmpjhqy0pwb9m8wm19l0f463xla54frd";
+  };
+
+in stdenv.mkDerivation rec {
   name = "vault-${version}";
   version = "0.9.3";
 
@@ -14,9 +22,11 @@ stdenv.mkDerivation rec {
     sha256 = "0angapmlzm4xr724wj17d7siywfh0x6k215llvlrf0v5ja8xprng";
   };
 
-  phases = "unpackPhase";
-  unpackPhase = ''
+  phases = "installPhase";
+  installPhase = ''
     mkdir -p $out/bin
+    mkdir -p $out/share/bash-completion/completions
     cp $src/vault $out/bin/vault
-    '';
+    cp ${vaultBashCompletions}/vault-bash-completion.sh $out/share/bash-completion/completions/vault
+  '';
 }
