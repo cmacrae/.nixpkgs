5e42b77bf19499e24645bf12f86cb2cd7ecf5231
Calum MacRae <calum.macrae@moo.com>
2018-11-22 15:46:49 +0000

    overlays: Update up

--- overlays/up/deps.nix
+++ overlays/up/deps.nix
@@ -1,56 +1,65 @@
 [
-  {
-    goPackagePath = "github.com/gdamore/encoding";
-    fetch = {
-      type = "git";
-      url = "https://github.com/gdamore/encoding";
-      rev = "b23993cbb6353f0e6aa98d0ee318a34728f628b9";
-      sha256 = "0d7irqpx2fa9vkxgkhf04yiwazsm10fxh0yk86x5crflhph5fv8a";
-    };
-  }
-  {
-    goPackagePath = "github.com/gdamore/tcell";
-    fetch = {
-      type = "git";
-      url = "https://github.com/gdamore/tcell";
-      rev = "8e3efba0949258012c8853626356bec2f716b56c";
-      sha256 = "0knwy90ffbjiafmndv2g06ybzc265qx6h6gxj4572g4ha9pg8va4";
-    };
-  }
-  {
-    goPackagePath = "github.com/lucasb-eyer/go-colorful";
-    fetch = {
-      type = "git";
-      url = "https://github.com/lucasb-eyer/go-colorful";
-      rev = "c7842319cf3ac2eff253e8b3ebe15fcc56b6414a";
-      sha256 = "00v2x6qchhi6vv09w29kcyr9i0kq4n4daaj3vk1c4nfj2z7xx5hc";
-    };
-  }
-  {
-    goPackagePath = "github.com/mattn/go-isatty";
-    fetch = {
-      type = "git";
-      url = "https://github.com/mattn/go-isatty";
-      rev = "3fb116b820352b7f0c281308a4d6250c22d94e27";
-      sha256 = "084hplr4n4g5nvp70clljk428hc963460xz0ggcj3xdi4w7hhsvv";
-    };
-  }
-  {
-    goPackagePath = "github.com/mattn/go-runewidth";
-    fetch = {
-      type = "git";
-      url = "https://github.com/mattn/go-runewidth";
-      rev = "b20a3daf6a39840c202fd42cc23d53607162b045";
-      sha256 = "0crivpncmh22696d5cy7k15ll5yqfjcigk0xy73wb6g1q6vnfxs7";
-    };
-  }
-  {
-    goPackagePath = "golang.org/x/text";
-    fetch = {
-      type = "git";
-      url = "https://go.googlesource.com/text";
-      rev = "4d1c5fb19474adfe9562c9847ba425e7da817e81";
-      sha256 = "1y4rf9cmjyf8r56khr1sz0chbq1v0ynaj63i2z1mq6k6h6ww45da";
-    };
-  }
+    {
+	goPackagePath = "github.com/gdamore/encoding";
+	fetch = {
+	    type = "git";
+	    url = "https://github.com/gdamore/encoding";
+	    rev = "b23993cbb6353f0e6aa98d0ee318a34728f628b9";
+	    sha256 = "0d7irqpx2fa9vkxgkhf04yiwazsm10fxh0yk86x5crflhph5fv8a";
+	};
+    }
+    {
+	goPackagePath = "github.com/gdamore/tcell";
+	fetch = {
+	    type = "git";
+	    url = "https://github.com/gdamore/tcell";
+	    rev = "8e3efba0949258012c8853626356bec2f716b56c";
+	    sha256 = "0knwy90ffbjiafmndv2g06ybzc265qx6h6gxj4572g4ha9pg8va4";
+	};
+    }
+    {
+	goPackagePath = "github.com/lucasb-eyer/go-colorful";
+	fetch = {
+	    type = "git";
+	    url = "https://github.com/lucasb-eyer/go-colorful";
+	    rev = "c7842319cf3ac2eff253e8b3ebe15fcc56b6414a";
+	    sha256 = "00v2x6qchhi6vv09w29kcyr9i0kq4n4daaj3vk1c4nfj2z7xx5hc";
+	};
+    }
+    {
+	goPackagePath = "github.com/mattn/go-isatty";
+	fetch = {
+	    type = "git";
+	    url = "https://github.com/mattn/go-isatty";
+	    rev = "3fb116b820352b7f0c281308a4d6250c22d94e27";
+	    sha256 = "084hplr4n4g5nvp70clljk428hc963460xz0ggcj3xdi4w7hhsvv";
+	};
+    }
+    {
+	goPackagePath = "github.com/mattn/go-runewidth";
+	fetch = {
+	    type = "git";
+	    url = "https://github.com/mattn/go-runewidth";
+	    rev = "b20a3daf6a39840c202fd42cc23d53607162b045";
+	    sha256 = "0crivpncmh22696d5cy7k15ll5yqfjcigk0xy73wb6g1q6vnfxs7";
+	};
+    }
+    {
+	goPackagePath = "github.com/spf13/pflag";
+	fetch = {
+	    type = "git";
+	    url = "https://github.com/spf13/pflag";
+	    rev = "aea12ed6721610dc6ed40141676d7ab0a1dac9e9";
+	    sha256 = "17p5k37bnzj6wfh000y7xpvxyv2wsfa3db9sm8da2frjvn7jgbp2";
+	};
+    }
+    {
+	goPackagePath = "golang.org/x/text";
+	fetch = {
+	    type = "git";
+	    url = "https://go.googlesource.com/text";
+	    rev = "4d1c5fb19474adfe9562c9847ba425e7da817e81";
+	    sha256 = "1y4rf9cmjyf8r56khr1sz0chbq1v0ynaj63i2z1mq6k6h6ww45da";
+	};
+    }
 ]
