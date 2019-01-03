40b70bbadb4376d0a4591fd1f66275271660d87f
Calum MacRae <calum.macrae@moo.com>
2019-01-03 09:26:53 +0000

    apps: Upgrade iTerm to 3.2.6

--- pkgs/apps.nix
+++ pkgs/apps.nix
@@ -39,11 +39,11 @@ installApplication =
     inherit (builtins) replaceStrings;
     name = "iTerm2";
     appname = "iTerm";
-    version = "3.2.0";
+    version = "3.2.6";
     sourceRoot = "iTerm.app";
     src = super.fetchurl {
     url = "https://iterm2.com/downloads/stable/iTerm2-${replaceStrings ["\."] ["_"] version}.zip";
-      sha256 = "19121a3hdqvsm6l778s7myfm8z61ss8c0g8rlwlvypbfdybn4j3x";
+      sha256 = "116qmdcbbga8hr9q9n1yqnhrmmq26l7pb5lgvlgp976yqa043i6v";
     };
     description = "iTerm2 is a replacement for Terminal and the successor to iTerm";
     homepage = https://www.iterm2.com;
